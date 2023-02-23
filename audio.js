6d8ab99df18234fc6a17eb13e649de1bd9b65dc2
Ailadir <117507118+Ailadir@users.noreply.github.com>
2023-02-24 01:01:20 +0300

    One Piece Landing Page v0.9.7 1. Added JS for fading effects of some text.

--- audio.js
+++ audio.js
@@ -1,11 +1,30 @@
+var mysong = new Audio();
+mysong.src = './ff2.mp3';
 
-var mysong= new Audio()
-mysong.src= "./ff2.mp3";
+const playsong = () => {
+  mysong.play();
+};
 
-const playsong = () =>{
-    mysong.play();
-}
+const pausesong = () => {
+  mysong.pause();
+};
 
-const pausesong = () =>{
-    mysong.pause();
-}
+const toHide = document.querySelector('.luffy_text');
+const toShow = document.querySelector('.luffy_quote');
+window.addEventListener('scroll', () => {
+  const fade_time = window.innerHeight / 7;
+  const show_time = window.innerHeight * 1.35;
+  const scrolled = window.scrollY;
+  console.log(`Window height ${show_time}`);
+  console.log(scrolled);
+  if (scrolled > fade_time) {
+    toHide.classList.add('fade');
+  } else {
+    toHide.classList.remove('fade');
+  }
+  if (scrolled > show_time) {
+    toShow.classList.add('fade_in');
+  } else {
+    toShow.classList.remove('fade_in');
+  }
+});
